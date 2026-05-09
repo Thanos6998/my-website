e18a6a4a4cce1d240b90139584b732a5592a819d
Jeewan Poudel <jeewanp245@gmail.com>
2026-05-09 21:25:29 +0545

    feat: cloudinary config

--- my-website-main/backend/cloudinary_config.py
+++ my-website-main/backend/cloudinary_config.py
@@ -1,7 +1,14 @@
 import cloudinary
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent
+load_dotenv(ROOT_DIR / '.env')
 
 cloudinary.config(
-    cloud_name="dlfxabo9b",
-    api_key="147234592376233",
-    api_secret="wlfRn6mAps9A5ogfdAg_xb68Hww"
+    cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME", "dlfxabo9b"),
+    api_key=os.environ.get("CLOUDINARY_API_KEY", "147234592376233"),
+    api_secret=os.environ.get("CLOUDINARY_API_SECRET", "wlfRn6mAps9A5ogfdAg_xb68Hww"),
+    secure=True
 )
